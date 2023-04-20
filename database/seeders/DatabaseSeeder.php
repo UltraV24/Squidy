b3a4e4b3701eddffe229c909881e44d2791e48ea
unknown <irvihavizi@gmail.com>
2023-04-20 21:08:45 +0700

    tugas 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this -> call(usertabelpost::class);
+        $this -> call(PostsTableSeeder::class);
     }
 }
